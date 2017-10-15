e32da806ad21956f894199bade4acf525a923ffd
Jean <jeanfilho@gmail.com>
2017-10-15 15:06:05 +0200

    Added 2 simple tutorials and started extensive tutorial

---
+++ Assets/ProceduralGenerationTutorial/Scripts/Noise.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public static class Noise  {
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        if (scale <= 0.0001f)
+            scale = 0.0001f;
+
+        for(int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for(int i = 0; i < octaves; i++)
+                { 
+                    float sampleX = x / scale * frequency;
+                    float sampleY = y / scale * frequency;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        return noiseMap;
+    }
+     
+}
